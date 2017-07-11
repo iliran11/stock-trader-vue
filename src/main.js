27723134b7d1206e3a1f9de6ecb956a9289960cc
iliran11 <yh6mphx5>
2017-07-12 00:01:43 +0300

    added testing - part 2

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import store from './store/store.js'
+import store from './store/store'
+import { sync } from 'vuex-router-sync'
+import VueHead from 'vue-head'
 
-Vue.config.productionTip = false
+sync(store, router)
+Vue.use(VueHead)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App },
-  store
+  store,
+  render: h => h(App)
 })
--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
-import Hello from '@/components/Hello'
+import stockNew from '@/components/stockNew'
 
-describe('Hello.vue', () => {
+describe('newStock', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(Hello)
+    const Constructor = Vue.extend(stockNew)
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('.hello h1').textContent)
       .to.equal('Welcome to Your Vue.js App')
